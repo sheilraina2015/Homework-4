510d4f8f2a0b7701575bb0e75d31463da67af329
sheilraina2015 <sheilraina2015@gmail.com>
2025-10-14 18:52:58 +0300

    yes12

---
+++ extra.py
@@ -0,0 +1,22 @@
+print(' i will ask you for 2 numbers')
+num1 = int(input('type a number'))
+num2 = int(input('another please'))
+print(num1 + num2) 
+print(num1 * num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)    
+print(num1 ** num2)
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 >= num2)
+print(num1 <= num2)   
+print(num1 < num2)
+print(num1 > num2)
+
+ 
+
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+num = int(input('tell me a number'))
+ex = num**2 
+print('the number you gave me before, times itself is',ex )
+print('that numbers square root is :',num)
+print('your welcome for nothing  (:')
