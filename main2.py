edd1cbdc5465ac7de718e8a7188ddffbba4a5810
sheilraina2015 <sheilraina2015@gmail.com>
2025-10-17 10:40:37 +0300

    69

--- main2.py
+++ main2.py
@@ -1,19 +1,19 @@
-print(' i will ask you for 2 numbers')
-num1 = int(input('type a number'))
-num2 = int(input('another please'))
-print(num1 + num2) 
+print('I will ask you for 2 numbers')
+
+num1 = int(input('Type a number: '))
+num2 = int(input('Another please: '))
+
+print(num1 + num2)
 print(num1 * num2)
 print(num1 - num2)
 print(num1 / num2)
 print(num1 // num2)
-print(num1 % num2)    
+print(num1 % num2)
 print(num1 ** num2)
-
 print(num1 == num2)
 print(num1 != num2)
 print(num1 >= num2)
-print(num1 <= num2)   
+print(num1 <= num2)
 print(num1 < num2)
 print(num1 > num2)
 
-print(num1 = num2)
